26c8feef7caf566ca2a13cbb57fe40eac85e3da0
Samuel Hybois <samuel.hybois@ensam.eu>
2018-12-06 10:14:57 +0100

    Ajout d'un test + message d'erreur si la variable d'entrée n'est pas un nombre entier

--- factoriel.m
+++ factoriel.m
@@ -2,10 +2,16 @@ function f = factoriel(n)
 % input : n, nombre entier
 % output : f, nombre entier
 
-if n>2
+if floor(n) ~= n
+	disp('n doit être entier')
+else
+
+	if n>2
 		f=1;
 	else
 		f = n*factoriel(n-1);
+	end
+	
 end
 
 end
